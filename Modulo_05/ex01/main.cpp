b6ff1a56c638743db1bda06dfa4d24bd29e4da7a
jorgeedualves <jorgeedualves84@gmail.com>
2023-05-02 11:07:48 -0300

    Modulo05 - ex01 ok

--- Modulo_05/ex01/main.cpp
+++ Modulo_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 19:15:56 by joeduard          #+#    #+#             */
-/*   Updated: 2023/04/28 22:27:31 by joeduard         ###   ########.fr       */
+/*   Updated: 2023/05/02 11:07:10 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,105 +73,6 @@ int main(void)
     {
 		std::cout << "**Form 4**\n";
         std::cerr << exc.what() << std::endl;
-    }
-
-    // try
-    // {
-    //     bureaucrat = new Bureaucrat("Joao 3", 2);
-    //     form = new Form("Form 3", 1, 1);
-    //     bureaucrat->decrementGrade();
-    //     std::cout << "Bureaucrat: " << *bureaucrat << " -- Grade: " << bureaucrat->getGrade();
-    //     std::cout << "\nFormulario: " << *form << " -- Signed: " << form->getSigned() << " -- Grade to sign: "
-    //     << form->getGradeSign() << " -- Grade to Execute: " << form->getGradeExecute() << "\n"; 
-    //     delete bureaucrat;
-    //     delete form;    
-    // }
-    // catch (std::exception & exc)
-    // {
-    //     std::cerr << exc.what() << std::endl;
-    // }
-
-    // try
-    // {
-    //     bureaucrat = new Bureaucrat("Joao 4", 3);
-    //     std::cout << "Bureaucrat: " << *bureaucrat << " -- Grade: " << bureaucrat->getGrade() << "\n";
-    //     delete bureaucrat;
-    //     form = new Form("Form high", -1, 1);
-    //     delete form;  
-    // }
-    // catch(const std::exception& exc)
-    // {
-    //     std::cerr << exc.what() << std::endl;
-    // }
-
-    // try
-    // {
-    //     bureaucrat = new Bureaucrat("Raquel", 1);
-    //     std::cout << "Bureaucrat: " << *bureaucrat << " -- Grade: " << bureaucrat->getGrade() << "\n";
-    //     delete bureaucrat;
-    //     form = new Form("Form high", -1, 1);
-    //     delete form;  
-    // }
-    // catch(const std::exception& exc)
-    // {
-	// 	std::cout << "**catch Raquel**\n";
-    //     std::cerr << exc.what() << std::endl;
-    // }
-
-	// try
-    // {
-    //     bureaucrat = new Bureaucrat("Rafaela", 1);
-    //     std::cout << "Bureaucrat: " << *bureaucrat << " -- Grade: " << bureaucrat->getGrade() << "\n";
-    //     delete bureaucrat;
-    //     form = new Form("Form high", 151, 1);
-    //     delete form;  
-    // }
-    // catch(const std::exception& exc)
-    // {
-	// 	std::cout << "**catch Rafaela**\n";
-    //     std::cerr << exc.what() << std::endl;
-    // }
-    
-
+    }   
     return (0);
 }    
-
-
-
-//     try
-//     {
-//         bureaucrat = new Bureaucrat("Rafaela", 1);
-//         bureaucrat->incrementGrade();
-//         std::cout << "Bureaucrat : "<< *bureaucrat << "\nGrade: " << bureaucrat->getGrade() << "\n";
-//         delete bureaucrat;    
-//     }
-//     catch (std::exception & exc)
-//     {
-//         std::cerr << exc.what() << std::endl;
-//     }
-
-//     try
-//     {
-//         bureaucrat = new Bureaucrat("Rosana", 150);
-//         bureaucrat->decrementGrade();
-//         std::cout << "Bureaucrat : "<< *bureaucrat << "\nGrade: " << bureaucrat->getGrade() << "\n";
-//         delete bureaucrat;    
-//     }
-//     catch (std::exception & exc)
-//     {
-//         std::cerr << exc.what() << std::endl;
-//     }
-
-//      try
-//     {
-//         bureaucrat = new Bureaucrat("Rita", 151);
-//         std::cout << "Bureaucrat : "<< *bureaucrat << "\nGrade: " << bureaucrat->getGrade() << "\n";
-//         delete bureaucrat;    
-//     }
-//     catch (std::exception & exc)
-//     {
-//         std::cerr << exc.what() << std::endl;
-//     }
-    
-//     return (0);
-// }
