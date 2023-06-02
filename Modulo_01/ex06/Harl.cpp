29f41fdb0a71d0b757add5adee5b2b2db1269c29
jorgeedualves <jorgeedualves84@gmail.com>
2023-06-02 19:36:28 -0300

    tirando fallthrough do switch

--- Modulo_01/ex06/Harl.cpp
+++ Modulo_01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:32:28 by joeduard          #+#    #+#             */
-/*   Updated: 2023/06/02 12:59:39 by joeduard         ###   ########.fr       */
+/*   Updated: 2023/06/02 19:31:07 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ void Harl::debug(void)
 {
     std::cout << "[ DEBUG ]\n" <<
     "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger."
-    " I really do!\n";
+    " I really do!\n\n";
 }
 
 void Harl::info(void)
 {
     std::cout << "[ INFO ]\n" <<
     "I cannot believe adding extra bacon costs more money. You didn't put"
-    " enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+    " enough bacon in my burger! If you did, I wouldn't be asking for more!\n\n";
 }
 
 void Harl::warning(void)
 {
     std::cout << "[ WARNING ]\n" <<
     "I think I deserve to have some extra bacon for free.\n" <<
-    "I've been coming for years whereas you started working here since last month.\n";
+    "I've been coming for years whereas you started working here since last month.\n\n";
 }
 
 void Harl::error(void)
@@ -58,29 +58,29 @@ void Harl::complain(std::string level)
 
     switch (opt)
     {
-    case 0:
-    { (this->*ptFuncs[0])();
-      std::cout << std::endl;
-      __attribute__((fallthrough));
-    }
-     case 1:
-    { (this->*ptFuncs[1])();
-      std::cout << std::endl;
-      __attribute__((fallthrough));
-    }
-     case 2:
-    { (this->*ptFuncs[2])();
-      std::cout << std::endl;
-      __attribute__((fallthrough));
-    }
-     case 3:
-    { (this->*ptFuncs[3])();
-      std::cout << std::endl;
-      break;
-    }
-    default:
-        std::cout << "[ Probably complaining about insignificant problems ]\n";
+      case 0:
+      { 
+        (this->*ptFuncs[0])();
+      }
+      
+      case 1:
+      { 
+        (this->*ptFuncs[1])(); 
+      }
+      
+      case 2:
+      {
+        (this->*ptFuncs[2])();
+      }
+
+      case 3:
+      {
+        (this->*ptFuncs[3])();
         break;
+      }
+      default:
+          std::cout << "[ Probably complaining about insignificant problems ]\n";
+          break;
     }
 	return;
 }
