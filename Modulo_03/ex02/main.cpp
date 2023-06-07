0460b4da1a58aefc31eff8799f20f33c53295567
jorgeedualves <jorgeedualves84@gmail.com>
2023-06-07 10:46:49 -0300

    Modulo03 - ex02: Atualização do Main

--- Modulo_03/ex02/main.cpp
+++ Modulo_03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 13:29:56 by joeduard          #+#    #+#             */
-/*   Updated: 2023/06/05 21:47:57 by joeduard         ###   ########.fr       */
+/*   Updated: 2023/06/07 10:44:15 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,50 @@
 
 int main(void)
 {
-  ClapTrap clapTrap("Megazord");
-  ScavTrap scavTrap("Daimon");
-  FragTrap fragTrap("Robocop");
+    std::cout << "1 " << std::endl;
+    ClapTrap clapTrap("Megazord");
+    ScavTrap scavTrap("Daimon");
+    FragTrap fragTrap("Robocop");
   
-  std::cout << "\n";
-  std::cout << clapTrap << "\n";
-  std::cout << scavTrap << "\n";
-  std::cout << fragTrap<< "\n";
+    std::cout << "\n";
+    std::cout << "2 " << std::endl;
+    std::cout << clapTrap << "\n";
+    std::cout << scavTrap << "\n";
+    std::cout << fragTrap<< "\n";
 
-  clapTrap.attack(scavTrap.getName());
-  scavTrap.takeDamage(clapTrap.getAD());
-  clapTrap.beRepaired(50);
-  std::cout << "\n";
+    std::cout << "3 " << std::endl;
+    clapTrap.attack(scavTrap.getName());
+    scavTrap.takeDamage(clapTrap.getAD());
+    clapTrap.beRepaired(50);
+    std::cout << "\n";
 
-  scavTrap.attack(clapTrap.getName());
-  clapTrap.takeDamage(scavTrap.getAD());
-  scavTrap.guardGate();
-  scavTrap.beRepaired(50);
-  std::cout << "\n";
+    std::cout << "4 " << std::endl;
+    scavTrap.attack(clapTrap.getName());
+    clapTrap.takeDamage(scavTrap.getAD());
+    scavTrap.guardGate();
+    scavTrap.beRepaired(10);
+    std::cout << "\n";
 
-  fragTrap.attack(scavTrap.getName());
-  scavTrap.takeDamage(fragTrap.getAD());
-  fragTrap.highFivesGuys();
-  fragTrap.beRepaired(40);
-  std::cout << "\n";
+    std::cout << "5 " << std::endl;
+    fragTrap.attack(scavTrap.getName());
+    scavTrap.takeDamage(fragTrap.getAD());
+    fragTrap.highFivesGuys();
+    fragTrap.beRepaired(40);
+    std::cout << "\n";
 
-  std::cout << clapTrap << "\n";
-  std::cout << scavTrap << "\n";
-  std::cout << fragTrap << "\n";
+    std::cout << "6 " << std::endl;
+    fragTrap.attack(scavTrap.getName());
+    scavTrap.takeDamage(fragTrap.getAD());
+    fragTrap.attack(scavTrap.getName());
+    scavTrap.takeDamage(fragTrap.getAD());
+    fragTrap.attack(scavTrap.getName());
+    scavTrap.takeDamage(fragTrap.getAD());
+    std::cout << "\n";
 
-  return (0);
+    std::cout << "7 " << std::endl;
+    std::cout << clapTrap << "\n";
+    std::cout << scavTrap << "\n";
+    std::cout << fragTrap << "\n";
+
+    return (0);
 }
