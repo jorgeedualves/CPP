b54c665315e7139c5e57a16660538e335207ff68
jorgeedualves <jorgeedualves84@gmail.com>
2023-07-21 14:39:00 -0300

    correção ex01 para template não const

--- Modulo_07/ex01/Iter.hpp
+++ Modulo_07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 13:21:35 by joeduard          #+#    #+#             */
-/*   Updated: 2023/07/20 17:13:55 by joeduard         ###   ########.fr       */
+/*   Updated: 2023/07/21 13:45:51 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,7 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* array, size_t lenght, void (*f)(const T&))
-{   
-    if(!array || !f)
-        return;
-    for(size_t i = 0; i < lenght; i++)
-        f(array[i]);
-}
-
-template <typename T>
-void iter(T* array, size_t lenght, void (*f)(T&))
+void iter(T* array, size_t lenght, void (*f)(T const &))
 {   
     if(!array || !f)
         return;
@@ -37,6 +28,15 @@ template <typename T>
 void printArray(const T& n)
 {
     std::cout << n << " ";
+}
+
+template <typename T>
+void iter(T* array, size_t lenght, void (*f)(T&))
+{   
+    if(!array || !f)
+        return;
+    for(size_t i = 0; i < lenght; i++)
+        f(array[i]);
 }
 
 #endif
--- Modulo_07/ex01/main.cpp
+++ Modulo_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 13:21:47 by joeduard          #+#    #+#             */
-/*   Updated: 2023/07/20 17:11:31 by joeduard         ###   ########.fr       */
+/*   Updated: 2023/07/21 14:06:16 by joeduard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,52 +34,63 @@ void print( T& x )
   return;
 }
 
-int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };
-  Awesome tab2[5];
+int main( void )
+{
+	{
+		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int length = sizeof(array) / sizeof(array[0]);
 
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
+    std::cout << "\n[** TEST INT ARRAY **]";
+		std::cout << std::endl << "Int   array: ";
 
-  return 0;
+		iter(array, length, printArray<int>); 
+
+		std::cout << std::endl;
+	}
+
+	{
+		float array[] = {1.4, 3.2, 5.9, 7.8, 9, 10};
+		int length = sizeof(array) / sizeof(array[0]);
+
+    std::cout << "\n[** TEST FLOAT ARRAY **]";
+		std::cout << std::endl << "Float array: ";
+
+		iter(array, length, printArray); 
+
+  		std::cout << std::endl;
+	}
+
+	{
+		char array[] = {'z', 'e', 'g', 'r', 't', 'u', 'y', 'l', 'a', 'q', 'i'};
+		int length = sizeof(array) / sizeof(array[0]);
+
+    std::cout << "\n[**TEST CHAR ARRAY**]";
+		std::cout << std::endl << "Char array: ";
+
+		iter(array, length, printArray<char>);
+
+  	std::cout << std::endl << std::endl;
+	}
+
+  {
+    std::string array[] = {"jorge", "Eduardo", "Alves"};
+		int length = sizeof(array) / sizeof(array[0]);
+
+    std::cout << "\n[**TEST STRING ARRAY**]";
+		std::cout << std::endl << "String array: ";
+
+		iter(array, length, printArray<std::string>);
+
+  	std::cout << std::endl << std::endl;
+	}
+
+  {
+    int tab[] = { 0, 1, 2, 3, 4 };
+    Awesome tab2[5];
+
+    std::cout << "[** TEST AWESOME **]\n";
+    iter( tab, 5, print<const int> );
+    iter( tab2, 5, print<Awesome> );
+  }
+    return (0);
 }
-
-
-
-// int main( void )
-// {
-// 	{
-// 		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-// 		int length = sizeof(array) / sizeof(array[0]);
-
-// 		std::cout << std::endl << "Int   array: ";
-
-// 		iter(array, length, printArray<int>); 
-
-// 		std::cout << std::endl;
-// 	}
-
-// 	{
-// 		float array[] = {1.4, 3.2, 5.9, 7.8, 9, 10};
-// 		int length = sizeof(array) / sizeof(array[0]);
-
-// 		std::cout << std::endl << "Float array: ";
-
-// 		iter(array, length, printArray); 
-
-//   		std::cout << std::endl;
-// 	}
-
-// 	{
-// 		char array[] = {'z', 'e', 'g', 'r', 't', 'u', 'y', 'l', 'a', 'q', 'i'};
-// 		int length = sizeof(array) / sizeof(array[0]);
-
-// 		std::cout << std::endl << "Char  array: ";
-
-// 		iter(array, length, printArray<char>);
-
-//   		std::cout << std::endl << std::endl;
-// 	}
-
-//     return (0);
-// }
